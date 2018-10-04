30fe36837e070929a79836580f70742bd5ae953a
Ksenia <ksenia.lavruk@yandex.ru>
2018-10-05 02:58:41 +0300

    Commit header

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
-	/* Call plugins hrere ...*/
 
-	
-});
+	//Плавная прокрутка по ссылкам 
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
+
+
+}); 
