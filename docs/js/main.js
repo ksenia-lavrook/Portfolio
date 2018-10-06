ce64371b04232ac2d0fed130cfff4a0cb63f7dab
Ksenia <ksenia.lavruk@yandex.ru>
2018-10-06 21:40:22 +0300

    Commit filtres

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
 	//Плавная прокрутка по ссылкам 
-	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	$("nav a,a[href='#top'], a[href='#resume'], a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
 	    highlightSelector:"nav a"
 	});
 
